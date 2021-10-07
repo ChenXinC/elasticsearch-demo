1206e6328ce1eabc9c24cfd9b5400acfe70a072d
chenxin <823877739@qq.com>
2021-10-07 23:32:16 +0800

    init

--- src/main/java/com/cx/elasticsearch/ElasticsearchDemoApplication.java
+++ src/main/java/com/cx/elasticsearch/ElasticsearchDemoApplication.java
@@ -1,4 +1,4 @@
-package com.cx.elasticsearch.demo;
+package com.cx.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
